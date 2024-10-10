72d9025dc980b82414ba04424e3ddbf01428b7c8
Extra_Nuclear <cclarke99999@icloud.com>
2024-10-10 17:24:31 +0100

    Lots Of Good Changes 🪲

--- Main Program/Main Program.py
+++ Main Program/Main Program.py
@@ -29,16 +29,28 @@ while run == True:
         os.system("cls")
 
         if choice == 1:
-            CALLSIGN = input("CALLSIGN = ")
-            STAND = input("STAND = ")
-            DEPARTURE = input("DEPARTURE = ")
-            ARRIVAL = input("ARRIVAL = ")
-            INFO = input("ATIS INFO = ")
-            GROUNDFRQ = input("GROUND FREQUENCY")
+            CALLSIGN = input("CALLSIGN = ").upper()
+            STAND = input("STAND = ").upper()
+            DEPARTURE = input("DEPARTURE = ").upper()
+            ARRIVAL = input("ARRIVAL = ").upper()
+            INFO = input("ATIS INFO = ").upper()
+            print(f"\n")
+            print("F̲r̲e̲q̲u̲e̲n̲c̲i̲e̲s̲: ")
+            GROUNDFRQ = input("GROUND FREQUENCY").upper()
 
 
         elif choice == 2:
             print(f"{DEPARTURE} Delivery, good day, {CALLSIGN} at stand {STAND}, with information {INFO}, requesting clearance to {ARRIVAL}")
+            SID = input("DEPARTURE SID = ")
+            RUNWAY = int(input("RUNWAY = "))
+            SQUAWK = int(input("SQUAWK = "))
+            input("---PRESS--ENTER--TO--CONTINUE---")
+            print(f"Cleared to {ARRIVAL}, {SID} departure, runway {RUNWAY}, squawk {SQUAWK}, {CALLSIGN}")
+            print("(1) Readback Correct")
+            print("(2) Change Something")
+            mini_choice = int(input("= "))
+            print(f"Report when ready, {CALLSIGN}")
+
 
         elif choice == 3:
             stage = "Push & Start"
